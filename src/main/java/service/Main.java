1a521cb4708bfdf4e49d3f6364cf3a91b630afc8
jwsj <alenkasubina@gmail.com>
2023-11-17 01:39:51 +0300

    Добавлен вызов из main

--- src/main/java/service/Main.java
+++ src/main/java/service/Main.java
@@ -1,10 +1,18 @@
 package service;
 
 import service.converters.Converter;
+import service.converters.JsonToXml;
+
+import javax.xml.bind.JAXBException;
+import java.io.FileNotFoundException;
 
 public class Main {
-    public static void main(String[] args) {
-
-
+    public static void main(String[] args) throws JAXBException, FileNotFoundException {
+        String fileName = "C:\\Users\\Алёна\\OneDrive\\Рабочий стол\\Практика\\FileConverterService\\src\\main\\resources\\"
+                + "Json" + ".json";
+        String fileName2 = "C:\\Users\\Алёна\\OneDrive\\Рабочий стол\\Практика\\FileConverterService\\src\\main\\resources\\"
+                + "h1" + ".xml";
+        JsonToXml s = new JsonToXml();
+        s.Convert(fileName, fileName2);
     }
 }
